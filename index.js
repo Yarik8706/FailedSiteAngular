12da21099d0caf8463a13b60b7b3fa8107224508
Yaroslav <77915716+Yarik8706@users.noreply.github.com>
2021-08-26 10:53:38 +0300

    debug

--- index.js
+++ index.js
@@ -6,29 +6,13 @@ const bodyParser = require('body-parser')
 const path = require('path')
 
 const app = express()
-// app.use(function (req, res, next) {
-//
-//     // Website you wish to allow to connect
-//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-//
-//     // Request methods you wish to allow
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//
-//     // Request headers you wish to allow
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-//
-//     // Set to true if you need the website to include cookies in the requests sent
-//     // to the API (e.g. in case you use sessions)
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//
-//     // Pass to next layer of middleware
-//     next();
-// })
 
 app.use(bodyParser.json({ limit: '1000mb' }))
 
 // use it before all route definitions
 app.use(cors());
+
+app.use(express.json())
 
 app.use('/login', require('./routes/auth.routes'))
 
